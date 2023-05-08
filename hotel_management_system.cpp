9574f20f4598d68ae9c1b81bc5438cbad5a84efa
melikenuralan <131565055+melikenuralan@users.noreply.github.com>
2023-05-08 03:24:15 +0300

    cpp

--- hotel_management_system.cpp
+++ hotel_management_system.cpp
@@ -1,12 +1,28 @@
 #include<iostream>
 
 using namespace std;
+bool exitProgram = false;
 
-int main()
-{
 	int quant; //quantity
-	int choice;
+	int choice=0;
 	
+	void appColor();
+	
+	void admin_login();
+	void user_login();
+	void login();
+	
+	
+	void room();
+	void pasta();
+	void burger();
+	void noodle();
+	void shake();
+	void chicken();
+	void info();
+	void exitProgramFunc();
+	
+
 	 //quantity of items
 	int Qrooms = 0 , Qpasta = 0 , Qburger = 0 ,Qnoodles = 0 , Qshake = 0 , Qchicken = 0;
 	//sold items
@@ -14,6 +30,52 @@ int main()
 
 	//total price of items
 	int Total_rooms = 0 , Total_pasta = 0 , Total_burger = 0 , Total_noodles = 0 , Total_shake = 0 , Total_chicken = 0 ;
+	
+	void appColor()
+{
+    system("Color 0C");
+    cout<<endl;
+    for(int i=1;i<31;i++)
+    {
+    	cout << char(223);
+	}
+    cout <<char(219)<<"       WELCOME TO THE HOTEL         "<<char(219);
+
+    for(int j=1;j<31;j++)
+    {
+    	cout << char(223);
+	}
+}
+	
+	void login(){
+		
+		int x;
+	appColor();
+	cout<<endl<<endl;
+    cout << "Please choose an option:\n";
+    cout << "1. Admin Login \n";
+    cout << "2. User Login\n";
+    cin >> x;
+
+  	  switch (x) {
+   	     case 1:
+    	        admin_login();
+   	         break;
+    	 case 2:
+        	    user_login();
+            	break;
+        	default:
+            	cout << "Invalid option \n";
+            	break;
+	}
+	  getchar();
+        cout<<"\n\nPress [ENTER] to go back to the main menu...";          
+        getchar();
+        system("cls");
+		
+	}
+	
+	void admin_login(){
 	
 	cout<<"\n Quantity of items we have";
 	cout<<"\n\n Rooms avaible : ";
@@ -28,10 +90,14 @@ int main()
 	cin>>Qshake;
 	cout<<"\n Quantity of chicken-roll : ";
 	cin>>Qchicken;
-
-//MENU
-
-m: // label m
+	
+		
+	}
+	
+	
+	void user_login(){
+		
+	system("cls");
 	cout<<"\n\t\t\t Please select from the menu options ";
 	cout<<"\n\n 1.Rooms :";
 	cout<<"\n 2.Pasta : ";
@@ -42,101 +108,156 @@ m: // label m
 	cout<<"\n 7.information regarding sales and collection : ";
 	cout<<"\n 8.Exit : ";
 	
-	cout<<"please enter your choice between (1-8)";
+	cout<<"please enter your choice between (1-8) : ";
 	cin>>choice;
 	
 	switch(choice)
 	{
-		case 1: cout<<"\n\nEnter the numbers of rooms you want : ";
+		case 1:  room();
+							break;
+		case 2:  pasta();
+							break;
+						
+		case 3:  burger();
+							break;
+							
+		case 4: noodle();
+							break;
+							
+		case 5: shake();
+							break;
+									
+		case 6:  chicken();
+							break;
+							
+		case 7:  info();
+						
+						break;
+						
+		case 8: exitProgramFunc();
+						break;
+							
+						
+	default:
+				cout<<"\n please select the numbers mention above : ";
+
+				
+	}
+
+	
+		
+	}
+	
+	int main()
+	{
+		void appColor();
+		 while(!exitProgram) {
+			 login();
+}
+
+
+		
+		return 0;
+	}
+	
+	void room(){
+		cout<<"\n\nEnter the numbers of rooms you want : ";
 					cin>>quant;
 						if(Qrooms-Srooms >= quant )
 						{
 							Srooms = Srooms + quant;
 							Total_rooms = Total_rooms+quant*1200;
-							cout<<"\n\n\t\t"<<quant<<"room/rooms have been alloted to you !!";	
+							cout<<"\n\n\t"<<quant<<" room/rooms have been alloted to you !!";	
 							
 						}
 						else{
-							cout<<"\n\t Only"<<Qrooms-Srooms<<"Rooms remaining in hotel ";
+							cout<<"\n\t Only "<<Qrooms-Srooms<<" Rooms remaining in hotel ";
 					
 						}
-							break;
-		case 2: cout<<"\n\nEnter pasta quantity : ";
+		
+	}
+	void pasta(){
+		cout<<"\n\nEnter pasta quantity : ";
 					cin>>quant;
 						if(Qpasta-Spasta >= quant )
 						{
 							Spasta = Spasta + quant;
 							Total_pasta = Total_pasta+quant*250;
-							cout<<"\n\n\t\t"<<quant<<"pasta is the order !";		
+							cout<<"\n\n\t"<<quant<<" pasta is the order !";		
 							
 						}
 						else{
-							cout<<"\n\t Only"<<Qpasta-Spasta<<"pasta stock ";
+							cout<<"\n\t Only "<<Qpasta-Spasta<<" pasta stock ";
 					
 						}
-							break;
-						
-						case 3: cout<<"\n\nEnter burger quantity : ";
+		
+	}
+	void burger(){
+		cout<<"\n\nEnter burger quantity : ";
 					cin>>quant;
 						if(Qburger-Sburger >= quant )
 						{
 							Sburger = Sburger + quant;
 							Total_burger= Total_burger+quant*120;
-							cout<<"\n\n\t\t"<<quant<<"burger is the order !!";	
+							cout<<"\n\n\t"<<quant<<" burger is the order !!";	
 							
 						}
 						else{
-							cout<<"\n\t Only"<<Qburger-Sburger<<"burger stock";
+							cout<<"\n\t Only "<<Qburger-Sburger<<" burger stock";
 					
 						}
-							break;
-							
-			case 4: cout<<"\n\nEnter noodles quantity : ";
+		
+	}
+	void noodle(){
+		cout<<"\n\nEnter noodles quantity : ";
 					cin>>quant;
 						if(Qnoodles-Snoodles >= quant )
 						{
 							Snoodles = Snoodles + quant;
 							Total_noodles = Total_noodles+quant*250;
-							cout<<"\n\n\t\t"<<quant<<"noodles is the order !";		
+							cout<<"\n\n\t"<<quant<<" noodles is the order !";		
 							
 						}
 						else{
-							cout<<"\n\t Only"<<Qnoodles-Snoodles<<"noodles stock ";
+							cout<<"\n\t Only "<<Qnoodles-Snoodles<<" noodles stock ";
 					
 						}
-							break;
-							
-			case 5: cout<<"\n\nEnter shakes quantity : ";
+		
+	}
+	void shake(){
+		cout<<"\n\nEnter shakes quantity : ";
 					cin>>quant;
 						if(Qshake-Sshake >= quant )
 						{
 							Sshake = Sshake + quant;
 							Total_shake = Total_shake+quant*250;
-							cout<<"\n\n\t\t"<<quant<<"shake is the order !";		
+							cout<<"\n\n\t"<<quant<<" shake is the order !";		
 							
 						}
 						else{
-							cout<<"\n\t Only"<<Qshake-Sshake<<"shakes stock ";
+							cout<<"\n\t Only "<<Qshake-Sshake<<" shakes stock ";
 					
 						}
-							break;
-									
-				case 6: cout<<"\n\nEnter chicken quantity : ";
+		
+	}
+	void chicken(){
+		cout<<"\n\nEnter chicken quantity : ";
 					cin>>quant;
 						if(Qchicken-Schicken >= quant )
 						{
 							Schicken = Schicken + quant;
 							Total_chicken = Total_chicken+quant*250;
-							cout<<"\n\n\t\t"<<quant<<"chicken is the order !";		
+							cout<<"\n\n\t"<<quant<<" chicken is the order !";		
 							
 						}
 						else{
-							cout<<"\n\t Only"<<Qchicken-Schicken<<"chicken stock ";
+							cout<<"\n\t Only "<<Qchicken-Schicken<<" chicken stock ";
 					
 						}
-							break;
-							
-				case 7: cout<<"\n\tDetails of sales and collection ";
+		
+	}
+	void info(){
+		cout<<"\n\tDetails of sales and collection ";
 						cout<<"\n\nNumber of rooms we had :  "<<Qrooms;
 						cout<<"\n\nNumber of rooms we gave for rent : "<<Srooms;
 						cout<<"\n\nRemaining rooms : "<<Qrooms-Srooms;
@@ -168,18 +289,14 @@ m: // label m
 						cout << "\n\nRemaining chickens: " << Qchicken - Schicken;
 						cout << "\n\nTotal chicken collection for the day: " << Total_chicken;
 						
-						cout<<"\n\n\n Total collection for the day : "<<Total_rooms+Total_pasta+Total_burger+Total_noodles+Total_shake+Total_chicken;
-						
-						break;
-						
-						case 8:
-							exit(0);
-						
-						default:
-							cout<<"\n please select the numbers mention above : ";
-
-				
+		cout<<"\n\n\n Total collection for the day : "<<Total_rooms+Total_pasta+Total_burger+Total_noodles+Total_shake+Total_chicken;
+		
 	}
-goto m;
+void exitProgramFunc(){
+		
+		exit(0);
+		
+	}
 	
-}
+	
+	
